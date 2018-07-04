4cf0ffcb810f504326982493bd08b0d2b1f11f3d
SHIELD0616 <37204189+SHIELD0616@users.noreply.github.com>
2018-07-04 14:40:15 +0800

    Update Clock.cpp

--- Clock.cpp
+++ Clock.cpp
@@ -1,4 +1,23 @@
 class Clock {
+public:
+		void tick()
+		{
+			
+		}
+		
+		void show()
+		{
+			printf("%02d:%02d:%02d\n",hour,minute,second);
+			return;
+		}
+		
+		void adjust(int h,int m,int s)
+		{
+			hour=h,minute=m,second=s;
+			return;
+		}
+private:
+int hour,minute,second;
 };
 
 int main() {
