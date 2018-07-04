ba167121cb983fe7e32d06b62c857f8ab6fd7373
SHIELD0616 <37204189+SHIELD0616@users.noreply.github.com>
2018-07-04 14:57:02 +0800

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -1,15 +1,57 @@
+void in_prime()
+{
+	is_prime[1]=0;
+	for(int i=2;i<100000;i++)
+	{
+		is_prime[i]=!is_prime[i];
+		if(is_prime[i])
+			for(int j=i+i;j<100000;j+=i)
+				is_prime[j]=1;
+	}
+}
+
 class Prime {
-public:
-  Prime(int num);
-  ~Prime();
+	public:
+		Prime(int num):mNum(num){}
+		~Prime(){}
 
-  bool isPrime();
+		bool isPrime()
+		{
+			return is_prime[mNum];
+		}
 
-  Prime sumBit();
-  Prime multiBit();
-  Prime sqaureSumBit();
-private:
-  const int mNum;
+		Prime sumBit()
+		{
+			int sum=0,tmp_mNum=mNum;
+			while(tmp_mNum!=0)
+			{
+				sum+=tmp_mNum%10;
+				tmp_mNum/=10;
+			}
+			return Prime(sum);
+		}
+		Prime multiBit()
+		{
+			int product=1,tmp_mNum=mNum;
+			while(tmp_mNum!=0)
+			{
+				product*=tmp_mNum%10;
+				tmp_mNum/=10;
+			}
+			return Prime(product);
+		}
+		Prime sqaureSumBit()
+		{
+			int sum=0,tmp_mNum=mNum;
+			while(tmp_mNum!=0)
+			{
+				sum+=(tmp_mNum%10)*(tmp_mNum%10);
+				tmp_mNum/=10;
+			}
+			return Prime(sum);
+    }
+  private:
+		const int mNum;
 };
 
 int main() {
@@ -18,6 +60,6 @@ int main() {
   Prime mb = pm.multiBit();
   Prime ssb = pm.sqaureSumBit();
   if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime())
-    ; //to do sth
+    printf("Found Super Prime %d\n",i); //to do sth
   return 0;
 }
